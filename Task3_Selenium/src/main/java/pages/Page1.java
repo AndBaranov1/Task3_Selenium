230eec544b593fdd9024d9c12bd3bdd51ffe2f59
AndBaranov1 <37299544+AndBaranov1@users.noreply.github.com>
2018-06-23 09:38:06 +0700

    Update Page1.java

--- Task3_Selenium/src/main/java/pages/Page1.java
+++ Task3_Selenium/src/main/java/pages/Page1.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 /**
  * Created by 1 on 08.06.2018.
  */
-public class Page1 {
+public class Blanck1Page {
 
     @FindBy(xpath = "//DIV[@ng-click='setProdProg(prodProg)']")
     public WebElement titlesumma;
@@ -20,7 +20,7 @@ public class Page1 {
     @FindBy(xpath = "//SPAN[@ng-click='save()'][text()='Оформить']")
     public WebElement issueButton;
 
-    public Page1(WebDriver driver){
+    public Blanck1Page(WebDriver driver){
         PageFactory.initElements(driver, this);
         Wait<WebDriver> wait = new WebDriverWait(driver,10, 1000);
         wait.until(ExpectedConditions.visibilityOf(titlesumma));
